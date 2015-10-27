39ab043c9e6cbece1f7597e56c224403a8c03508
Andy Priebe <priebea@comcast.net>
2015-10-26 20:34:24 -0700

    cleaned up variable names in js; added an image to index; corrected js file to display all entries; changed index file to display entries as bullets; changed headers to link to bootstrap styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#favorite1").val();
-    var person2Input = $("input#favorite2").val();
-    var animalInput= $("input#favorite3").val();
-    var exclamationInput = $("input#favorite4").val();
-    var verbInput = $("favorite5").val();
+    var fav1Input = $("input#favorite1").val();
+    var fav2Input = $("input#favorite2").val();
+    var fav3Input= $("input#favorite3").val();
+    var fav4Input = $("input#favorite4").val();
+    var fav5Input = $("input#favorite5").val();
 
-    $(".favorite1").text(person1Input);
-    $(".favorite2").text(person2Input);
-    $(".favorite3").text(animalInput);
-    $(".favorite4").text(exclamationInput);
-    $(".favorite5").text(verbInput);
+    $(".favorite1").text(fav1Input);
+    $(".favorite2").text(fav2Input);
+    $(".favorite3").text(fav3Input);
+    $(".favorite4").text(fav4Input);
+    $(".favorite5").text(fav5Input);
 
     $("#list").show();
 
